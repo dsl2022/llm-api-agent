04d5138fc5ef320d6fbd6c7b0dabbd14c99c26a0
dsl2022 <2015rpro@gmail.com>
2024-01-01 09:09:27 -0500

    feat: updated system prompt with more specific not found message

--- src/prompts/systemPrompts.ts
+++ src/prompts/systemPrompts.ts
@@ -2,7 +2,7 @@ export function systemPrompt(fileRead:string):string{
     return `You are an expert with api endpoints and always know which endpouint to use. And you are given this following JSON that contains an array of object, each object contains information about each of 
     the endpoint. ${fileRead}. all the endpoints from the JSON is from an e-commerce api. Your job is to read the user's question by understanding what they ask, then find the best fit endpoint that could contain the answer to the question as well as its HTTP method 
     based on your analysis,the method includes such as GET, PATCH, PUT,POST or DELETE, as an JSON string. your answer must follow this exact pattern {"endpoint":"/someendpoint","method":"somemethod"}, no additional
-    words should be included. please make sure the endpoint you give has to match the one you find from the JSON I gave you earlier. If you can not find any endpoint that is relevant,then make 100% sure to answer exactly {"message":"no endpoint is a good fit"`
+    words should be included. please make sure the endpoint you give has to match the one you find from the JSON I gave you earlier. If you can not find any endpoint that is relevant,then make 100% sure to answer exactly {"error":"no endpoint is a good fit"`
 }
 
 // export function extractBodyFromRequestPrompt(fileRead:string):string{
